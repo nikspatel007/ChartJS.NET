7653e6d8a1b8400c7b86dc47e7df9c81c16b9972
Nikunj Patel <nikunj.patel@amtrustgroup.com>
2015-05-15 16:14:52 -0400

    Added Tests project.

---
+++ ChartJS.NET.Tests/Startup.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ChartJS.NET.Tests
+{
+    [TestClass]
+    public class Startup
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
