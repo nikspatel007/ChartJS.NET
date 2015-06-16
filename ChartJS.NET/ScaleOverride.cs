db3d810a9469aedc397962355ff144f302c06f64
= <jim.piller@AmtrustGroup.com>
2015-06-16 16:03:44 -0400

    Created global settings

---
+++ ChartJS.NET/ScaleOverride.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChartJS.NET
+{
+    public class ScaleOverride
+    {
+        /// <summary>
+        /// The number of steps in hard coded scale
+        /// </summary>
+        public string Steps { get; set; }
+
+        /// <summary>
+        /// The value jump in a hard coded scale
+        /// </summary>
+        public string StepWidth { get; set; }
+
+        /// <summary>
+        /// The scale start value in a hard coded scale
+        /// </summary>
+        public string StartValue { get; set; }
+
+
+    }
+}
