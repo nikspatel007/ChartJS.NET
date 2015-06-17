7beca521730ac060214db3a1113d09228e8bb0ff
Jim Piller <jim.piller@AmtrustGroup.com>
2015-06-17 11:12:53 -0400

    Added the samples MVC project

---
+++ ChartJSNet.Sample/Controllers/ErrorController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Mvc;
+
+namespace ChartJSNet.Sample.Controllers
+{
+    public class ErrorController : Controller
+    {
+        public ActionResult Http404()
+        {
+            Response.StatusCode = 404;
+
+            return View();
+        }
+    }
+}
