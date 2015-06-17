95ffced683261bda3bcc1be02ab572dd8d562701
Jim Piller <jim.piller@AmtrustGroup.com>
2015-06-17 11:04:56 -0400

    Started building the HTML Extension

---
+++ ChartJS.NET/Charts/CanvasProperties.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChartJS.NET.Charts
+{
+    public class CanvasProperties
+    {
+        public int Height { get; set; }
+        public int Width { get; set; }
+        public string CanvasId { get; set; }
+        public string CssClass { get; set; }
+    }
+}
---
+++ ChartJS.NET/Extensions/HtmlExtensions.cs
@@ -0,0 +1,55 @@
+﻿using ChartJS.NET.Charts;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace ChartJS.NET.Extensions
+{
+    public static class HtmlExtensions
+    {
+        public static MvcHtmlString CreateChart<TChartOptions>(this HtmlHelper helper, 
+            BaseChart<TChartOptions> chartOptions) where TChartOptions : GlobalOptions
+        {
+
+            string dataSets = string.Format(@"[{{{0}}}]", string.Join(",", chartOptions.Data));
+            string labels = string.Join(",", chartOptions.Labels);
+
+            string canvasMarkup = string.Format("<canvas id='{0}' width='{1}' height='{2}' class='{3}'></canvas>",
+                chartOptions.CanvasProperties.CanvasId, chartOptions.CanvasProperties.Width, chartOptions.CanvasProperties.Height,
+                chartOptions.CanvasProperties.CssClass);
+
+            var chart = string.Format(@"
+                {0}
+                <script type='text/javascript'>
+                    var ctx = document.getElementById({1}).getContext('2d');
+                    var data = {{
+                        labels: [{2}],
+                        datasets: {3}
+                    }};
+
+                    var chartManager = new Chart(ctx).{4}(data, {5});
+                </script>", canvasMarkup, chartOptions.CanvasProperties.CanvasId, labels, dataSets, chartOptions.ChartType.ToString(), 
+                          chartOptions.ChartConfig.ToJson());
+
+            MvcHtmlString output = new MvcHtmlString(chart);
+
+            return output;
+        }
+
+        private static string ToJson<T>(this T obj)
+        {
+            JsonSerializerSettings settings = new JsonSerializerSettings() 
+            { 
+                ContractResolver = new CamelCasePropertyNamesContractResolver(), 
+                NullValueHandling = NullValueHandling.Ignore 
+            };
+
+            return JsonConvert.SerializeObject(obj, settings);
+        }
+    }
+}
