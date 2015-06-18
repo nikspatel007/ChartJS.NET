b88bb2c496549cf6b330aae07bafd2c9ee9d0c20
Jim Piller <jim.piller@AmtrustGroup.com>
2015-06-18 13:18:25 -0400

    Added the base pieces for Pie charts

---
+++ ChartJS.NET/Charts/PieDoughnut/Pie.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChartJS.NET.Charts.PieDoughnut
+{
+    public class Pie : BaseChart<PieDataSet, PieChartOptions>
+    {
+        private readonly PieChartOptions _chartOptions = new PieChartOptions();
+
+        public override ChartTypes ChartType { get { return ChartTypes.Pie; } }
+
+        public override PieChartOptions ChartConfig
+        {
+            get { return _chartOptions; }
+        }
+    }
+}
