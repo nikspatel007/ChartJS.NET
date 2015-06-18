eecb81d352a7aee2655501ae56dbc29912751f65
Nikunj Patel <nikunj.patel@amtrustgroup.com>
2015-06-18 15:40:09 -0400

    needs work.

---
+++ ChartJS.NET/Charts/Radar/RadarChart.cs
@@ -0,0 +1,17 @@
+﻿namespace ChartJS.NET.Charts.Radar
+{
+    public class RadarChart : BaseChart<RadarDataSet, RadarChartOptions>
+    {
+        private readonly RadarChartOptions _barChartOptions = new RadarChartOptions();
+
+        public override ChartTypes ChartType
+        {
+            get { return ChartTypes.Radar; }
+        }
+
+        public override RadarChartOptions ChartConfig
+        {
+            get { return _barChartOptions; }
+        } 
+    }
+}
