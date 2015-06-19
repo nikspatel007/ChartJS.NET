e0c05da766559c2fb330d9ed1303a091b1d04d83
Jim Piller <jim.piller@AmtrustGroup.com>
2015-06-19 09:51:23 -0400

    Fixed bar chart not showing on the home page.

--- ChartJSNet.Sample/Controllers/HomeController.cs
+++ ChartJSNet.Sample/Controllers/HomeController.cs
@@ -21,27 +21,20 @@ namespace ChartJSNet.Sample.Controllers
                     StrokeColor = "rgba(220, 220, 220, 0.8)",
                     HighlightFillColor = "rgba(220, 220, 220, 0.75)",
                     HighlightStrokeColor = "rgba(220, 220, 220, 1)"
-                },
-                new BarDataSet() {
-                    Data = new double[] { 28, 48, 40, 19, 86, 27, 90 },
-                    Label = "Second Dataset",
-                    FillColor = "rgba(151,187,205, 0.5)",
-                    StrokeColor = "rgba(151,187,205, 0.8)",
-                    HighlightFillColor = "rgba(151,187,205, 0.75)",
-                    HighlightStrokeColor = "rgba(151,187,205, 1)"
-                }
+                }//,
+                //new BarDataSet() {
+                //    Data = new double[] { 28, 48, 40, 19, 86, 27, 90 },
+                //    Label = "Second Dataset",
+                //    FillColor = "rgba(151,187,205, 0.5)",
+                //    StrokeColor = "rgba(151,187,205, 0.8)",
+                //    HighlightFillColor = "rgba(151,187,205, 0.75)",
+                //    HighlightStrokeColor = "rgba(151,187,205, 1)"
+                //}
             };
 
             barChart.Data.Labels = new List<string>() {
                 "January", "February", "March", "April", "May", "June", "July"
             };
-
-
-            barChart.ChartConfig.BarDatasetSpacing = 50;
-            barChart.ChartConfig.ScaleGridLineColor = "rgba(0,0,0, 1)";
-            barChart.ChartConfig.ScaleGridLineWidth = 2;
-            barChart.ChartConfig.ScaleShowGridLines = true;
-            barChart.ChartConfig.ScaleShowHorizontalLines = true;
 
             return View(barChart);
         }
