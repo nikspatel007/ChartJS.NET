901b68833930f426b97367317ad8b0948f7dc663
Jim Piller <jim.piller@AmtrustGroup.com>
2015-06-19 09:33:06 -0400

    Added the doughnut chart

---
+++ ChartJS.NET/Charts/Doughnut/DoughnutChart.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ChartJS.NET.Infrastructure;
+
+namespace ChartJS.NET.Charts.Doughnut
+{
+    public class DoughnutChart : BaseChart<List<DoughnutChartData>, DoughnutChartOptions>
+    {
+        private readonly DoughnutChartOptions _chartOptions = new DoughnutChartOptions();
+
+        public override Enums.ChartTypes ChartType { get { return Enums.ChartTypes.Doughnut; } }
+
+        public override DoughnutChartOptions ChartConfig
+        {
+            get { return _chartOptions; }
+        }
+
+    }
+}
---
+++ ChartJSNet.Sample/Controllers/DoughnutChartController.cs
@@ -0,0 +1,41 @@
+﻿using ChartJS.NET.Charts.Doughnut;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ChartJS.NET.Infrastructure;
+
+namespace ChartJSNet.Sample.Controllers
+{
+    public class DoughnutChartController : Controller
+    {
+        public ActionResult Index()
+        {
+            DoughnutChart doughnutChart = new DoughnutChart();
+
+            doughnutChart.Data = new List<DoughnutChartData>(){
+                     new DoughnutChartData() {
+                    Value = 300,
+                    Color = "#F7464A",
+                    Highlight = "#FF5A5E",
+                    Label = "Red"
+                },
+                new DoughnutChartData() {
+                    Value = 50,
+                    Color = "#46BFBD",
+                    Highlight = "#5AD3D1",
+                    Label = "Green"
+                },
+                new DoughnutChartData() {
+                    Value = 100,
+                    Color = "#FDB45C",
+                    Highlight = "#FFC870",
+                    Label = "Yellow"
+                }
+            };
+
+            return View(doughnutChart);
+        }
+    }
+}
