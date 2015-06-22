d9e8612e4d18c5aed13ceeb406706c1da901a262
Jim Piller <jim.piller@AmtrustGroup.com>
2015-06-22 08:38:33 -0400

    Added the line chart examples along with highlighted code.  Options need to be finished.

--- ChartJSNet.Sample/ViewModels/chartViewModel.cs
+++ ChartJSNet.Sample/ViewModels/chartViewModel.cs
@@ -1,6 +1,37 @@
-﻿namespace ChartJS.NET.Sample.ViewModels
+﻿using ChartJS.NET.Charts.Bar;
+using ChartJS.NET.Charts.Doughnut;
+using ChartJS.NET.Charts.Line;
+using ChartJS.NET.Charts.Pie;
+using ChartJS.NET.Charts.PolarAreaChart;
+using ChartJS.NET.Charts.Radar;
+
+namespace ChartJS.NET.Sample.ViewModels
 {
     public class chartViewModel
     {
+        public chartViewModel()
+        {
+            LineChart = new LineChart();
+            LineChartWithOptions = new LineChart();
+            
+            BarChart = new BarChart();
+            PieChart = new PieChart();
+            DoughnutChart = new DoughnutChart();
+            PolarAreaChart = new PolarAreaChart();
+            RadarChart = new RadarChart();
+        }
+        
+        public LineChart LineChart { get; set; }
+        public LineChart LineChartWithOptions {get; set;}
+
+        public BarChart BarChart { get; set; }
+
+        public PieChart PieChart { get; set; }
+
+        public DoughnutChart DoughnutChart { get; set; }
+
+        public PolarAreaChart PolarAreaChart { get; set; }
+
+        public RadarChart RadarChart { get; set; }
     }
 }
