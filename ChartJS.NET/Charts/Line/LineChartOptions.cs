f0b5c89008bc8abf80368d0ae2a6bd62bb47c515
Jim Piller <jim.piller@AmtrustGroup.com>
2015-06-22 08:50:17 -0400

    Removed default option values - don't need them to build the base chart.

--- ChartJS.NET/Charts/Line/LineChartOptions.cs
+++ ChartJS.NET/Charts/Line/LineChartOptions.cs
@@ -3,29 +3,9 @@
     public class LineChartOptions
     {
         /// <summary>
-        ///     Default Values.
-        /// </summary>
-        public LineChartOptions()
-        {
-            ScaleShowGridLines = true;
-            ScaleGridLineColor = "rgba(0,0,0,0.05)";
-            ScaleGridLineWidth = 1;
-            ScaleShowHorizontalLines = true;
-            ScaleShowVerticalLines = true;
-            BeizerCurve = true;
-            BeizerCurveTension = 0.4;
-            PointDot = true;
-            PointDotRadius = 4;
-            PointDotStrokeWidth = 1;
-            PointHitDetectionRadius = 20;
-            DataSetStrokeWidth = 2;
-            DatasetFill = true;
-        }
-
-        /// <summary>
         ///     Whether grid lines are shown across the chart
         /// </summary>
-        public bool ScaleShowGridLines { get; set; }
+        public bool? ScaleShowGridLines { get; set; }
 
         /// <summary>
         ///     Colour of the grid lines; use rgba format - "rgba(0,0,0,.1)"
@@ -35,62 +15,62 @@
         /// <summary>
         ///     Width of the grid lines
         /// </summary>
-        public int ScaleGridLineWidth { get; set; }
+        public int? ScaleGridLineWidth { get; set; }
 
         /// <summary>
         ///     Whether to show horizontal lines (except X axis)
         /// </summary>
-        public bool ScaleShowHorizontalLines { get; set; }
+        public bool? ScaleShowHorizontalLines { get; set; }
 
         /// <summary>
         ///     Whether to show vertical lines (except Y axis)
         /// </summary>
-        public bool ScaleShowVerticalLines { get; set; }
+        public bool? ScaleShowVerticalLines { get; set; }
 
         /// <summary>
         ///     Whether the line is curved between points
         /// </summary>
-        public bool BeizerCurve { get; set; }
+        public bool? BeizerCurve { get; set; }
 
         /// <summary>
         ///     Tension of the bezier curve between points
         /// </summary>
-        public double BeizerCurveTension { get; set; }
+        public double? BeizerCurveTension { get; set; }
 
         /// <summary>
         ///     Whether to show a dot for each point
         /// </summary>
-        public bool PointDot { get; set; }
+        public bool? PointDot { get; set; }
 
         /// <summary>
         ///     Radius of each point dot in pixels
         /// </summary>
-        public int PointDotRadius { get; set; }
+        public int? PointDotRadius { get; set; }
 
         /// <summary>
         ///     Pixel width of point dot stroke
         /// </summary>
-        public int PointDotStrokeWidth { get; set; }
+        public int? PointDotStrokeWidth { get; set; }
 
         /// <summary>
         ///     amount extra to add to the radius to cater for hit detection outside the drawn point
         /// </summary>
-        public int PointHitDetectionRadius { get; set; }
+        public int? PointHitDetectionRadius { get; set; }
 
         /// <summary>
         ///     Whether to show a stroke for datasets
         /// </summary>
-        public bool DatasetStroke { get; set; }
+        public bool? DatasetStroke { get; set; }
 
         /// <summary>
         ///     Pixel width of dataset stroke
         /// </summary>
-        public int DataSetStrokeWidth { get; set; }
+        public int? DataSetStrokeWidth { get; set; }
 
         /// <summary>
         ///     Whether to fill the dataset with a colour
         /// </summary>
-        public bool DatasetFill { get; set; }
+        public bool? DatasetFill { get; set; }
 
         /// <summary>
         ///     A legend template
